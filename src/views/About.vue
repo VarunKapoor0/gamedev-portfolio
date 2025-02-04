d573977c90b182f32d07f6f91946d5f181fe50b6
VarunKapoor0 <varunkap@usc.edu>
2025-02-04 13:57:30 -0800

    Text Update
    Updated Footer, About and Contact pages. Also updated the avatar.

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,17 +4,17 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>John Matrix</strong>, a former Delta Force operative, now a hobbyist lumberjack.<br/>
-        I spend my days living alone with my daughter Jenny, and cutting trees and carrying trunks around to keep in shape.
+        I'm <strong>Varun Kapoor</strong>, a current computer science grad student with about 4 years of experience as a software developer in the automotive inustry.<br/>
+        I also have a strong focus on game development, pursued during my Masters of Science course here at University of Southern Calfifornia.
       </div>
 
       <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for a summer internship</strong> as a game developer. You can reach me at <a href="mailto:varunkap@usc.edu">varunkap@usc.edu</a> or <router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
-      <img src="img/avatar.png" alt="Avatar of John" /> 
+      <img src="img/avatar.jpeg" alt="Avatar of Varun" /> 
     </div>
 
     <div style="clear:both"></div>
